ce3220349d8e09bfdc9d16a76ffb7506ac73fe8a
Bryan Berger <114026865+BryanBerger32@users.noreply.github.com>
2023-02-27 16:10:40 -0500

    Update MavenMovies_Final_Course_Project.sql

--- SQL Data Analysis/MavenMovies_Final_Course_Project.sql
+++ SQL Data Analysis/MavenMovies_Final_Course_Project.sql
@@ -1,7 +1,7 @@
 /* The situation: 
 You and your business partner were recently approached by another local business owner who is interested in purchasing Maven Movies.
 He primarily owns restaurants and bars, so he has lots of questions for you about your business and the rental business in general. 
-His offer seems very generousk, so you are going to entertain his questions.
+His offer seems very generous, so you are going to entertain his questions.
 
 /* 
 1. My partner and I want to come by each of the stores in person and meet the managers. 
